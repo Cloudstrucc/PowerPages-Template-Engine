ab8b19550060fd11d05167875631872f4f8685a0
Frederick Pearson <fpearson@cloudstrucc.com>
2026-03-25 03:44:25 -0400

    fixed for e-sign

--- BuildGCWEB/deploy-power-pages.sh
+++ BuildGCWEB/deploy-power-pages.sh
@@ -993,8 +993,8 @@ run_portal_template_install() {
 #####################################
 # STEP 1: EXTRACT GCWEB FILES
 #####################################
-    echo "Extracting theme files..."
-    unzip -o "$ZIP_FILE_PATH" -d "$EXTRACTION_PATH"
+    # echo "Extracting theme files..."
+    # unzip -o "$ZIP_FILE_PATH" -d "$EXTRACTION_PATH"
 
 
 # STEP 2: CREATE SNIPPETS
@@ -1005,14 +1005,14 @@ run_portal_template_install() {
 #####################################
 # STEP 3: CREATE TEMPLATES
 #####################################
-    echo "Writing templates..."
-    write_templates "$BASE_PATH_TEMPLATES"
+    # echo "Writing templates..."
+    # write_templates "$BASE_PATH_TEMPLATES"
 
 #####################################
 # STEP 4: UPDATE HOME PAGE COPY
 #####################################
-    echo "Updating home page..."
-    update_home_page "$PAGE_TEMPLATE_NAME_NEW_HOME"
+    # echo "Updating home page..."
+    # update_home_page "$PAGE_TEMPLATE_NAME_NEW_HOME"
     
 #####################################
 # STEP 5: CREATE WEB PAGES & WEB FILES
@@ -1021,13 +1021,13 @@ run_portal_template_install() {
     >&2 echo "DEBUG: HOME_PAGE_ID before write_hierarchy: $HOME_PAGE_ID"
     >&2 echo "DEBUG: Calling write_hierarchy with path: ${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}"
     
-    write_hierarchy "${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}" "$HOME_PAGE_ID"
+    # write_hierarchy "${EXTRACTION_PATH}${THEME_ROOT_FOLDER_NAME}" "$HOME_PAGE_ID"
 
 #####################################
 # STEP 6: UPSERT THE BASELINE STYLES REQUIRED BY POWER PAGES
 #####################################
     echo "Updating baseline styles..."
-    update_baseline_styles
+    # update_baseline_styles
     
     echo "Portal template installation complete!"
 }
